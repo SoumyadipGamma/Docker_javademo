61ee77fe3f35be5e242068950954b6fd017e787a
dipak <dipakongit>
2022-08-23 23:16:37 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int result = cal(5,5);
+        System.out.println("5 + 5 = "+result);
+    }
+
+    public static int cal(int x, int y)
+    {
+        return x+y;
+    }
+}
